26f74cdab7851210ba40a21a4d9f478b8afce03c
Reyes Telesforo Alan Mauricio <al221910354@gmail.com>
2020-11-04 22:18:07 -0600

    circle avatar

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -7,7 +7,35 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Avatar Page'),
+        actions: <Widget>[
+
+          Container(
+            padding: EdgeInsets.all(5),
+            child: CircleAvatar(
+            backgroundImage: NetworkImage('https://img.etimg.com/thumb/width-1200,height-900,imgsize-252561,resizemode-1,msid-59965878/magazines/panache/chester-benningtons-exploding-emotions-brought-life-to-linkin-park-classics.jpg'),
+            radius: 25.0,
+            ),
+          ),
+
+
+          Container(
+            margin: EdgeInsets.only(right: 10.0),
+            child: CircleAvatar(
+              child: Text('SL'),
+              backgroundColor: Colors.brown,
+            ),
+          )
+        ],
       ),
+      
+      body: Center(
+        child: FadeInImage(
+          image: NetworkImage('https://mariskalrock.com/wp-content/uploads/2017/06/Linkin-Park-Download-Madrid-2017.jpg'),
+          placeholder: AssetImage('assets/jar-loading.gif'),
+          fadeInDuration: Duration(milliseconds: 200 ), 
+        ),
+      ),
+
     );
   }
 }
