5720b953e9d4f3442e143277e4aec4c20f37982c
Reyes Telesforo Alan Mauricio <al221910354@gmail.com>
2020-11-06 22:37:11 -0600

    fin componentes

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,11 @@ import 'package:curso_flutter/src/routes/routes.dart';
 import 'package:flutter/material.dart';
 import 'package:curso_flutter/src/pages/alert_page.dart';
 //import 'package:curso_flutter/src/pages/avatar_page.dart';
- 
+ import 'package:flutter_localizations/flutter_localizations.dart';
+
+
+
+
 void main() => runApp(MyApp());
  
 class MyApp extends StatelessWidget {
@@ -14,6 +18,17 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Material App',
       debugShowCheckedModeBanner: false,
+      localizationsDelegates: [
+   GlobalMaterialLocalizations.delegate,
+   GlobalWidgetsLocalizations.delegate,
+   GlobalCupertinoLocalizations.delegate,
+ ],
+ supportedLocales: [
+    const Locale('en', ''), // English, no country code
+    const Locale('es', 'ES'), 
+    const Locale.fromSubtags(languageCode: 'zh'), // Chinese *See Advanced Locales below*
+    // ... other locales the app supports
+  ],
       //home: HomePage(),
       initialRoute: '/',
       routes: getAplicationRoutes(),
--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -1,9 +1,15 @@
+import 'package:curso_flutter/src/pages/animated_container.dart';
 import 'package:curso_flutter/src/pages/card_pages.dart';
+import 'package:curso_flutter/src/pages/input_page.dart';
 import 'package:flutter/material.dart';
 
 import 'package:curso_flutter/src/pages/alert_page.dart';
 import 'package:curso_flutter/src/pages/avatar_page.dart';
 import 'package:curso_flutter/src/pages/home_page.dart';
+import 'package:curso_flutter/src/pages/slider_page.dart';
+import 'package:curso_flutter/src/pages/listview_page.dart';
+
+
 
 Map<String, WidgetBuilder> getAplicationRoutes(){
 
@@ -12,6 +18,12 @@ Map<String, WidgetBuilder> getAplicationRoutes(){
         'alert' : ( BuildContext context ) => AlertPage(),
         'avatar': ( BuildContext context ) => AvatarPage(),
         'card' : ( BuildContext context ) => CardPage(),
+        'animatedContainer' : ( BuildContext context ) => AnimatedContainerPage(),
+        'inputs' : ( BuildContext context ) => InputPage(),
+        'slider' : ( BuildContext context ) => SliderPage(),
+        'list' : ( BuildContext context ) => ListaPage(),
+
+
   };
 
 }
